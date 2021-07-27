8a4fb3e8551803199a2009db4696df2b063018b1
Gaurav Singh <gauravthegeek@gmail.com>
2021-07-27 06:01:39 +0530

    Add main.cpp for Producer Consumer problem impl

---
+++ ProducerConsumerProblem/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * Author: Gaurav Singh
+ * Date : 27-07-2021
+ */
+
+#include <iostream>
+
+int main() {
+	using std::cout;
+	using std::cin;
+
+	int producerCount = 0, consumerCount = 0;
+
+	cout << "Enter the number of producers\n";
+	cin >> producerCount;
+
+	cout << "Enter the number of consumers\n";
+	cin >> consumerCount;
+
+	try {
+		ProducerConsumerImpl(producerCount, consumerCount);
+	}
+	catch(...) {
+		cout << "Some exception occurred in ProducerConsumerImpl\n";
+	}
+
+	return 0;
+}
