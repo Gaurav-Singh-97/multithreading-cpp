7df1173cfd6aec098d087ffc3a821750f134b570
Gaurav Singh <gauravthegeek@gmail.com>
2021-08-06 22:34:04 +0530

    Implement buffer functions

--- ProducerConsumerProblem/Consumer.cpp
+++ ProducerConsumerProblem/Consumer.cpp
@@ -8,3 +8,10 @@ std::string Consumer::generateId()
 {
 	return std::string();
 }
+
+void Consumer::removeFromBuffer(std::queue<int>& buffer)
+{
+	auto objRemoved = buffer.front();
+	buffer.pop();
+	//std::cout << "Object removed from buffer " << objRemoved.description() << "\n";
+}
--- ProducerConsumerProblem/Producer.cpp
+++ ProducerConsumerProblem/Producer.cpp
@@ -8,3 +8,10 @@ std::string Producer::generateId()
 {
 	return std::string();
 }
+
+void Producer::addToBuffer(std::queue<int>& buffer)
+{
+	auto objToAdd = produce();
+	buffer.push(objToAdd);
+	//std::cout << "Object added to buffer " << objToAdd.description() << "\n";
+}
